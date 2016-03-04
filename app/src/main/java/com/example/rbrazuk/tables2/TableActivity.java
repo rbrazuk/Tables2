248c1234e9eb0db0928412612100fffd0cc6fb6e
Ross Brazuk <rbrazuk@tonicdesign.com>
2016-03-04 11:43:35 -0500

    pulls from api, creates array of teams, about to create TableAdapter

--- app/src/main/java/com/example/rbrazuk/tables2/TableActivity.java
+++ app/src/main/java/com/example/rbrazuk/tables2/TableActivity.java
@@ -7,17 +7,27 @@ import android.widget.ListView;
 
 import com.google.gson.Gson;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.io.IOException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import okhttp3.Call;
+import okhttp3.Callback;
 import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 public class TableActivity extends AppCompatActivity {
 
     private Team[] mTeams;
+    private String leagueName;
 
     @Bind(R.id.lv_table) ListView mListView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +35,8 @@ public class TableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_table);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setTitle("");
+
 
         ButterKnife.bind(this);
 
@@ -39,10 +51,71 @@ public class TableActivity extends AppCompatActivity {
         OkHttpClient client = new OkHttpClient();
         Gson gson = new Gson();
 
+        Request request = new Request.Builder().url(tableUrl).addHeader("X-Auth-Token","95468454664049ca81f3fc478ede7ef2").build();
 
+        client.newCall(request).enqueue(new Callback() {
+            @Override
+            public void onFailure(Call call, IOException e) {
+                e.printStackTrace();
+            }
 
+            @Override
+            public void onResponse(Call call, Response response) throws IOException {
+                if (!response.isSuccessful()) throw new IOException("Unexpected code " + response);
+                String jsonData = response.body().string();
+                System.out.println(jsonData);
+
+                try {
+                    mTeams = parseJsonTeam(jsonData);
+                    leagueName = getLeagueName(jsonData);
+
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            getSupportActionBar().setTitle(leagueName);
+                        }
+                    });
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
 
 
 
     }
+
+    private String getLeagueName(String json) throws JSONException {
+        JSONObject table = new JSONObject(json);
+        String seasonName = table.getString("leagueCaption");
+
+        return seasonName;
+    }
+
+    private Team[] parseJsonTeam(String json) throws JSONException {
+        JSONObject table = new JSONObject(json);
+        JSONArray standing = table.getJSONArray("standing");
+
+        System.out.println(standing.length());
+
+        mTeams = new Team[standing.length()];
+
+        for (int i = 0; i < standing.length(); i++) {
+            JSONObject jsonTeam = standing.getJSONObject(i);
+            Team team = new Team();
+
+            team.setTeamName(jsonTeam.getString("teamName"));
+            team.setPosition(jsonTeam.getInt("position"));
+            team.setGoalDifference(jsonTeam.getInt("goalDifference"));
+            team.setPoints(jsonTeam.getInt("points"));
+
+            mTeams[i] = team;
+
+        }
+
+        return mTeams;
+    }
+
+    
 }
--- app/src/main/java/com/example/rbrazuk/tables2/TableAdapter.java
+++ app/src/main/java/com/example/rbrazuk/tables2/TableAdapter.java
@@ -1,7 +1,5 @@
 package com.example.rbrazuk.tables2;
 
-/**
- * Created by rossbrazuk1 on 3/4/16.
- */
+
 public class TableAdapter {
 }
