769b7d71f03f7442bbf66a32a7ec65801a59505b
Ross Brazuk <rbrazuk@tonicdesign.com>
2016-03-04 10:05:48 -0500

    up button functionality, added Team class

---
+++ app/src/main/java/com/example/rbrazuk/tables2/Team.java
@@ -0,0 +1,42 @@
+package com.example.rbrazuk.tables2;
+
+
+public class Team {
+
+    private int position;
+    private String teamName;
+    private int goalDifference;
+    private int points;
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getGoalDifference() {
+        return goalDifference;
+    }
+
+    public void setGoalDifference(int goalDifference) {
+        this.goalDifference = goalDifference;
+    }
+
+    public int getPoints() {
+        return points;
+    }
+
+    public void setPoints(int points) {
+        this.points = points;
+    }
+}
